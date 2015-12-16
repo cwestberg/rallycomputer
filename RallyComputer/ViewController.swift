abede7d6ded2d76df14e9d00117ab6f658098c19
Clarence Westberg <clarence.westberg@gmail.com>
2015-12-15 21:18:40 -0600

    connected speed picker

--- RallyComputer/ViewController.swift
+++ RallyComputer/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+    @IBOutlet weak var speedLbl: UILabel!
+
+    @IBOutlet weak var speedPicker: UIPickerView!
+    var pickerData: [String] = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Connect data:
+        speedPicker.delegate = self
+        speedPicker.dataSource = self
+        
+        pickerData = ["1", "2", "3", "4", "5", "6"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        speedLbl.text = pickerData[row]
+    }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
 
 }
 
